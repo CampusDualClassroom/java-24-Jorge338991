414fb0ccf34f4ad0286ed146c6bbaddaf27d15df
Jorge338991 <jorge338991@gmail.com>
2025-01-23 22:19:01 +0100

    Se añade solucion a Exercise24

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,24 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> queue = new LinkedList<>();
 
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+
+        return queue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll()); // poll recupera o objeto String e acto seguido sacao da lista
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> miLista = createQueue();
+        printAndEmptyQueue(miLista);
     }
 
 }
